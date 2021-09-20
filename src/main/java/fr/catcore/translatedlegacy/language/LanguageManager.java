ef7ddf362bf924be8139d03c7201c6d46496a5dc
Cat Core <arthur.koalas@free.fr>
2021-09-20 13:25:42 +0200

    fix most missing translation keys

--- src/main/java/fr/catcore/translatedlegacy/language/LanguageManager.java
+++ src/main/java/fr/catcore/translatedlegacy/language/LanguageManager.java
@@ -4,13 +4,10 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
 
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Properties;
 
 public class LanguageManager {
 
@@ -28,7 +25,7 @@ public class LanguageManager {
             JsonObject languageEntry = jsonObject.getAsJsonObject(code);
             CODE_TO_STORAGE.put(code, new OldTranslationStorage(languageEntry.get("name").getAsString(), languageEntry.get("region").getAsString()));
             try {
-                CODE_TO_STORAGE.get(code).load(LanguageManager.class.getResourceAsStream("/assets/modid/lang/" + code + ".lang"));
+                CODE_TO_STORAGE.get(code).load(LanguageManager.class.getResourceAsStream("/assets/modid/lang_default/" + code + ".lang"));
             } catch (IOException e) {
                 e.printStackTrace();
             }
