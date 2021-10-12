c38d777eee96e18e6f72027b1526df743a96cd91
Cat Core <arthur.koalas@free.fr>
2021-10-12 22:28:54 +0200

    Closes #1, #2

--- src/main/java/fr/catcore/translatedlegacy/font/BetterTextRenderer.java
+++ src/main/java/fr/catcore/translatedlegacy/font/BetterTextRenderer.java
@@ -12,9 +12,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.Buffer;
 import java.nio.IntBuffer;
+import java.util.Arrays;
 
 public class BetterTextRenderer {
-    private int[] CHARS_WIDTH_ASCII = new int[65536];
     public int[] imageInt = new int[256];
     private int[] anInt = new int[256];
     private IntBuffer intBuffer = class_214.method_745(1024);
@@ -26,19 +26,24 @@ public class BetterTextRenderer {
     private static final String str1 = "0123456789abcdef";
 
     private void loadFonts(GameOptions arg, TextureManager arg1) {
-        String a = str;
-
-        for (int a1 = 0; a1 < a.length(); a1++) {
-            for (int a2 = 0; a2 < str1.length(); a2++) {
-                loadFont(arg, arg1, a.charAt(a1), str1.charAt(a2));
-            }
+        for (int a1 = 0; a1 < 256; a1++) {
+            int j = a1 * 256;
+            loadFont(arg, arg1, j);
         }
     }
 
-    private void loadFont(GameOptions arg, TextureManager arg1, char a, char b) {
+    private static int getStart(byte size) {
+        return size >> 4 & 15;
+    }
+
+    private static int getEnd(byte size) {
+        return (size & 15) + 1;
+    }
+
+    private void loadFont(GameOptions arg, TextureManager arg1, int j) {
         BufferedImage fontImage;
         try {
-            fontImage = ImageIO.read(BetterTextRenderer.class.getResourceAsStream("/assets/minecraft/textures/font/unicode_page_" + a + b + ".png"));
+            fontImage = ImageIO.read(BetterTextRenderer.class.getResourceAsStream("/assets/minecraft/textures/font/unicode_page_" + String.format("%02x", j / 256) + ".png"));
         } catch (IOException var18) {
             throw new RuntimeException(var18);
         } catch (IllegalArgumentException e) {
@@ -47,90 +52,69 @@ public class BetterTextRenderer {
 
         int imageWidth = fontImage.getWidth();
         int imageHeight = fontImage.getHeight();
-        int[] pixels = new int[imageWidth * imageHeight];
-        fontImage.getRGB(0, 0, imageWidth, imageHeight, pixels, 0, imageWidth);
 
-        int fontBlockIndex = (str.indexOf(a) * 16) + str1.indexOf(b);
-        int fontBlock = fontBlockIndex * 256;
-        for(int unicodeId = 0; unicodeId < 256; ++unicodeId) {
-            int euclidean = unicodeId % GLYPH_HEIGHT;
-            int nonEuclidean = unicodeId / GLYPH_WIDTH;
+        if (imageWidth != 256 || imageHeight != 256) return;
 
-            int charWidth;
-            for(charWidth = 7; charWidth >= 0; --charWidth) {
-                int var12 = euclidean * 8 + charWidth;
-                boolean wipLine = true;
-
-                for(int charHeight = 0; charHeight < 8; ++charHeight) {
-                    int var15 = (nonEuclidean * 8 + charHeight) * imageWidth;
-                    int outOfBound = pixels[var12 + var15] & 255;
-                    if (outOfBound > 0) {
-                        wipLine = false;
-                        break;
-                    }
-                }
-
-                if (!wipLine) {
-                    break;
-                }
+        for (int k = 0; k < 256; k++) {
+            byte b = this.CHARS_WIDTH[j + k];
+            if (b != 0 && getStart(b) > getEnd(b)) {
+                this.CHARS_WIDTH[j + k] = 0;
             }
-
-            int unicode = unicodeId + fontBlock;
-            if (unicode == 32) {
-                charWidth = 2;
-            }
-
-            this.CHARS_WIDTH_ASCII[unicode] = charWidth + 2;
         }
+
+        int fontBlockIndex = j / 256;
 
         this.imageInt[fontBlockIndex] = arg1.glLoadImage(fontImage);
         this.anInt[fontBlockIndex] = class_214.method_741(288 + fontBlockIndex);
         Tessellator tessellator = Tessellator.INSTANCE;
 
         for(int unicodeId = 0; unicodeId < 256; ++unicodeId) {
-            GL11.glNewList(this.anInt[fontBlockIndex] + unicodeId + fontBlock, 4864);
+            GL11.glNewList(this.anInt[fontBlockIndex] + unicodeId + j, 4864);
             tessellator.start();
-            int height = unicodeId % 16 * GLYPH_HEIGHT;
-            int width = unicodeId / 16 * GLYPH_WIDTH;
-            float var26 = 7.99F;
-            float var28 = 0.0F;
-            float var30 = 0.0F;
+            if ((j + unicodeId) != 32) {
+                int height = unicodeId % 16 * GLYPH_HEIGHT;
+                int width = unicodeId / 16 * GLYPH_WIDTH;
+                float var26 = 7.99F;
+                float var28 = 0.0F;
+                float var30 = 0.0F;
 
-            tessellator.vertex(
-                    0.0D,
-                    0.0F + var26,
-                    0.0D,
-                    (float)height / 128.0F + var28,
-                    ((float)width + var26) / 128.0F + var30
-            );
+                tessellator.vertex(
+                        0.0D,
+                        0.0F + var26,
+                        0.0D,
+                        (float) height / 128.0F + var28,
+                        ((float) width + var26) / 128.0F + var30
+                );
 
-            tessellator.vertex(
-                    0.0F + var26,
-                    0.0F + var26,
-                    0.0D,
-                    ((float)height + var26) / 128.0F + var28,
-                    ((float)width + var26) / 128.0F + var30
-            );
+                tessellator.vertex(
+                        0.0F + var26,
+                        0.0F + var26,
+                        0.0D,
+                        ((float) height + var26) / 128.0F + var28,
+                        ((float) width + var26) / 128.0F + var30
+                );
 
-            tessellator.vertex(
-                    0.0F + var26,
-                    0.0D,
-                    0.0D,
-                    ((float)height + var26) / 128.0F + var28,
-                    (float)width / 128.0F + var30
-            );
+                tessellator.vertex(
+                        0.0F + var26,
+                        0.0D,
+                        0.0D,
+                        ((float) height + var26) / 128.0F + var28,
+                        (float) width / 128.0F + var30
+                );
 
-            tessellator.vertex(
-                    0.0D,
-                    0.0D,
-                    0.0D,
-                    (float)height / 128.0F + var28,
-                    (float)width / 128.0F + var30
-            );
+                tessellator.vertex(
+                        0.0D,
+                        0.0D,
+                        0.0D,
+                        (float) height / 128.0F + var28,
+                        (float) width / 128.0F + var30
+                );
+            }
+
 
             tessellator.draw();
 
-            GL11.glTranslatef(this.getCharWidth(unicodeId + fontBlock), 0.0F, 0.0F);
+            GL11.glTranslatef(this.getCharWidth(unicodeId + j), 0.0F, 0.0F);
             GL11.glEndList();
         }
 
@@ -159,7 +143,7 @@ public class BetterTextRenderer {
                 blue /= 4;
             }
 
-            GL11.glNewList(this.anInt[fontBlockIndex] + 256 + var21, 4864);
+            GL11.glNewList(this.anInt[0] + 256 + var21, 4864);
             GL11.glColor3f((float)red / 255.0F, (float)green / 255.0F, (float)blue / 255.0F);
             GL11.glEndList();
         }
@@ -200,19 +184,10 @@ public class BetterTextRenderer {
             GL11.glPushMatrix();
             GL11.glTranslatef((float)x, (float)y, 0.0F);
             for(int var12 = 0; var12 < string.length(); ++var12) {
-                ((Buffer)this.intBuffer).clear();
                 int var141 = BetterCharacterUtils.getId(string.charAt(var12));
-                for (int a1 = 0; a1 < str.length(); a1++) {
-                    boolean breack = false;
-                    for (int a2 = 0; a2 < str1.length(); a2++) {
-                        if ((var141 - ((a1*16) + a2)*256) <= 256) {
-                            GL11.glBindTexture(3553, this.imageInt[((a1*16) + a2)]);
-                            breack = true;
-                            break;
-                        }
-                    }
-                    if (breack) break;
-                }
+                if (this.CHARS_WIDTH[var141] == 0) continue;
+                ((Buffer)this.intBuffer).clear();
+                GL11.glBindTexture(3553, this.imageInt[var141 / 256]);
 
                 for(; string.length() > var12 + 1 && string.charAt(var12) == 167; var12 += 2) {
                     int var13 = "0123456789abcdef".indexOf(string.toLowerCase().charAt(var12 + 1));
@@ -230,17 +205,8 @@ public class BetterTextRenderer {
 
                 if (var12 < string.length()) {
                     int var14 = BetterCharacterUtils.getId(string.charAt(var12));
-                    for (int a1 = 0; a1 < str.length(); a1++) {
-                        boolean breack = false;
-                        for (int a2 = 0; a2 < str1.length(); a2++) {
-                            if ((var14 - ((a1*16) + a2)*256) <= 256) {
-                                this.intBuffer.put(this.anInt[((a1*16) + a2)] + var14);
-                                breack = true;
-                                break;
-                            }
-                        }
-                        if (breack) break;
-                    }
+                    if (this.CHARS_WIDTH[var14] <= 0) continue;
+                    this.intBuffer.put(this.anInt[var14 / 256] + var14);
                 }
 
                 if (this.intBuffer.remaining() == 0) {
@@ -275,17 +241,8 @@ public class BetterTextRenderer {
     }
     
     private int getCharWidth(int index) {
-        if (index == 32) return this.CHARS_WIDTH_ASCII[index];
-
-        int j = this.CHARS_WIDTH[index] >>> 4;
-        int k = this.CHARS_WIDTH[index] & 15;
-        if (k > 7) {
-            k = 15;
-            j = 0;
-        }
-
-        ++k;
-        return (k - j) / 2 + 1;
+        byte width = this.CHARS_WIDTH[index];
+        return index == 32 ? 4 : (getEnd(width) - getStart(width)) / 2 + 1;
     }
 
     private void loadGlyphSizes() {
