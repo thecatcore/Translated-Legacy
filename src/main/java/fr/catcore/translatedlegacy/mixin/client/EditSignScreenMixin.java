10a96ba92e016f48ebb954c6fb56375b6415908d
Cat Core <arthur.koalas@free.fr>
2022-03-17 18:43:51 +0100

    allow to write any character in chat and on sign

--- src/main/java/fr/catcore/translatedlegacy/mixin/client/EditSignScreenMixin.java
+++ src/main/java/fr/catcore/translatedlegacy/mixin/client/EditSignScreenMixin.java
@@ -13,4 +13,14 @@ public class EditSignScreenMixin {
     public String init$lang(String string) {
         return I18n.translate("gui.done");
     }
+
+    @ModifyArg(method = "keyPressed", index = 0, at = @At(value = "INVOKE", target = "Ljava/lang/String;indexOf(I)I"))
+    public int fixCharInput(int ch) {
+        return '0';
+    }
+
+    @ModifyArg(method = "render", index = 1, at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/EditSignScreen;drawTextWithShadowCentred(Lnet/minecraft/client/render/TextRenderer;Ljava/lang/String;III)V"))
+    public String render$lang(String string) {
+        return I18n.translate("sign.edit");
+    }
 }
