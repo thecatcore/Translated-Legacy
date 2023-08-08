1cb190592984327c74d4d2788f655b7ef81782cb
Cat Core <arthur.koalas@free.fr>
2023-08-08 19:04:36 +0200

    fix missing translations

--- babric/src/main/java/fr/catcore/translatedlegacy/babric/mixin/LevelMixin.java
+++ babric/src/main/java/fr/catcore/translatedlegacy/babric/mixin/LevelMixin.java
@@ -11,12 +11,12 @@ public class LevelMixin {
 
     @ModifyArg(method = "method_195", index = 0, at = @At(value = "INVOKE", target = "Lnet/minecraft/class_62;method_1795(Ljava/lang/String;)V"))
     private String saveLevel$translate(String string) {
-        return class_629.method_2049("menu.saving");
+        return class_629.method_2049("menu.savingLevel");
     }
 
 
     @ModifyArg(method = "method_195", index = 0, at = @At(value = "INVOKE", target = "Lnet/minecraft/class_62;method_1796(Ljava/lang/String;)V"))
     private String saveLevel$translate2(String string) {
-        return class_629.method_2049("menu.saving");
+        return class_629.method_2049("menu.savingChunks");
     }
 }
